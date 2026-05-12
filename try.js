9af45776b1404fb550a6b72fb99aba393fd89d81
Ishur1302 <ishansh4420@gmail.com>
2026-05-12 16:12:47 +0530

    feat: add MediaRecorder logic for real-time audio streaming

--- try.js
+++ try.js
@@ -4,3 +4,9 @@ socket.binaryType = 'arraybuffer';
 socket.onopen = () => {
     console.log('EchoBase Connection Established');
 };
+async function startStreaming() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = (e) => socket.send(e.data);
+    mediaRecorder.start(100); // Send chunks every 100ms
+}
